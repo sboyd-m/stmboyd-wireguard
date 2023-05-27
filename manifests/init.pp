ec8fa77568ad2d7afab0e96b31ff44417d20d801
sboyd-m <stmboyd@tutanota.com>
2023-05-26 17:50:59 -0700

    Manage service

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,10 +2,14 @@ class wireguard (
   Stdlib::IP::Address::V4::CIDR $local_addr4,
   Stdlib::IP::Address::V6::CIDR $local_addr6,
   String $package_name,
+  String $service,
   String $private_key,
   Array[Hash] $peers,
   Boolean $is_vpn_server = false,
 ) {
   class { 'wireguard::install': }
   -> class { 'wireguard::configure': }
+  ~> service { "${wireguard::service}@wg0.service":
+    ensure => running,
+  }
 }
